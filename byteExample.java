bba60052a84bf5b7cdf7a7f0c9d97890cac0ac3d
Yezyizhere01 <2631266284@qq.com>
2025-09-12 21:55:23 +0800

    learn byte

---
+++ byteExample.java
@@ -0,0 +1,9 @@
+public class byteExample {
+    // byte is a data type that can store whole numbers from -128 to 127
+    public static void main(String[] args){
+        byte minByte = -128;
+        byte maxByte = 127;
+        System.out.println("Minimum byte value: " + minByte);
+        System.out.println("Maximum byte value: " + maxByte);
+    }
+}
